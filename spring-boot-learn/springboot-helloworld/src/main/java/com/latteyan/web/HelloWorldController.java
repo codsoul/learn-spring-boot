f58346ef6a665ef95e65d19854587b049bddac4d
latteyan <latteyan@ideaculture.com>
2016-08-23 11:26:37 +0800

    [spring-boot-learn] - Hello world

---
+++ spring-boot-learn/springboot-helloworld/src/main/java/com/latteyan/web/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.latteyan.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+}
