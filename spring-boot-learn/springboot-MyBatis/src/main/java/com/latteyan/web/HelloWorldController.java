6f279e4be04b6e16d1cbcb29ff83666be78be93c
latteyan <latteyan@ideaculture.com>
2017-07-03 16:50:26 +0800

    update test

--- spring-boot-learn/springboot-MyBatis/src/main/java/com/latteyan/web/HelloWorldController.java
+++ spring-boot-learn/springboot-MyBatis/src/main/java/com/latteyan/web/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.latteyan.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.latteyan.dao.UserMapper;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("../hello")
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/hello")
     public String index() {
         return "Hello World";
     }
